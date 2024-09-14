2824507be53dec3c68f7965ba02ca7cd2e9b720e
00nateo <nateo@vt.edu>
2024-09-13 22:13:59 -0400

    Created Components
    Default Page is Sign In

---
+++ Hackathon24/src/Components/Page_Signin/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import React from 'react'
+
+const Page_Signin = () => {
+    return (
+        <>
+            <div>Hello from Sign In Component</div>
+        </>
+    )
+}
+export default Page_Signin
