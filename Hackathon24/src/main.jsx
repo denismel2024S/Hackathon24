f6d93c285f789f54332429f2210631b1975308e6
wulffandrew <165091432+wulffandrew@users.noreply.github.com>
2024-10-06 17:42:52 -0400

    PropelAuth

--- Hackathon24/src/main.jsx
+++ Hackathon24/src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from "@propelauth/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <AuthProvider authUrl="https://06398801081.propelauthtest.com">
+      <App/>
+    </AuthProvider>,
+    document.getElementById("root")
+);
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-) 
