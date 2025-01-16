1280538186027a585737804e09bf99d3203f7a1e
denismel2024S <denismel@vt.edu>
2025-01-16 17:19:27 -0500

    dark mode for map

--- Hackathon24/src/components/XUnused/xmap_with_marker/index.jsx
+++ Hackathon24/src/components/XUnused/xmap_with_marker/index.jsx
@@ -105,16 +105,76 @@ const MapWithMarker = ({ riderId, address, initialCoordinates, destination, dest
 //       setLoading(false);
 //     }
 //   };
-
+  const lightMode = [
+    {
+      featureType: 'water',
+      stylers: [{ color: '#b9d3c2' }],
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{ color: '#ffffff' }],
+    },
+    {
+      featureType: 'landscape',
+      stylers: [{ color: '#f2f2f2' }],
+    },
+    {
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }],
+    },
+  ];
+  const darkMode = [
+    {
+      elementType: 'geometry',
+      stylers: [{ color: '#212121' }],
+    },
+    {
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#757575' }],
+    },
+    {
+      elementType: 'labels.text.stroke',
+      stylers: [{ color: '#212121' }],
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{ color: '#393939' }],
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#212121' }],
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#8a8a8a' }],
+    },
+    {
+      featureType: 'water',
+      stylers: [{ color: '#000000' }],
+    },
+    {
+      featureType: 'landscape',
+      stylers: [{ color: '#2c2c2c' }],
+    },
+    {
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }],
+    },
+  ];
   if (!isLoaded) return <div>Loading map...</div>;
-
+  const isDark = true;
   return (
     <>
       <GoogleMap
         center={center}
         zoom={15}
         mapContainerStyle={{ width: '100%', height: '40rem' }}
-      >
+        options={{ styles: isDark ? darkMode : lightMode }}
+        >
         {markerPosition && (
           <Marker
             position={markerPosition}
